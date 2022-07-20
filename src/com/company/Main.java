46551ed86928feb275d0b548b75ecb935c3cfb08
soft_dev_hugo_ <A01228679@exatec.tec.mx>
2022-07-20 10:27:31 -0500

    Closable objects

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,15 @@ public class Main {
     public static void main(String[] args) {
         var file = new File("hello.txt");
         System.out.println("File exists: " + file.exists());
+        // If in documentation, you read closable, you are responsible for closing the object.
+        try (FileReader reader = new FileReader(file);
+             BufferedReader br = new BufferedReader(reader)){
+            var text = br.readLine();
+            System.out.println(text);
+        } catch (IOException e) {
+            //throw new RuntimeException(e);
+            e.printStackTrace();
+        }
     }
 
 }
